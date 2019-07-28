36635f0a95f37bea242444adf9cd42f4f6e386e1
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 17:30:42 +0200

    Update declarations.h

--- src/framework/thread/declarations.h
+++ src/framework/thread/declarations.h
@@ -3,5 +3,13 @@
 
 #include "../pch.h"
 
-namespace FW::Thread {}  // namespace FW::Thread
-#endif                   // #ifndef FW_THREAD_DECLARATIONS_H
+namespace FW::Thread {
+class Event;
+class EventPoll;
+class EventManager;
+
+typedef std::shared_ptr<Event> Event_ptr;
+typedef std::shared_ptr<EventPoll> EventPoll_ptr;
+typedef std::shared_ptr<EventManager> EventManager_ptr;
+}  // namespace FW::Thread
+#endif  // #ifndef FW_THREAD_DECLARATIONS_H
