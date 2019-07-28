e7dda2db19e63252b2ea15accf31d2c4ce445db2
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 17:47:32 +0200

    Create declarations.h

---
+++ src/framework/pymodule/declarations.h
@@ -0,0 +1,7 @@
+#ifndef FW_PYMODULE_DECLARATIONS_H
+#define FW_PYMODULE_DECLARATIONS_H
+
+#include "../pch.h"
+
+namespace FW::PyModule {}  // namespace FW::PyModule
+#endif                     // #ifndef FW_PYMODULE_DECLARATIONS_H
