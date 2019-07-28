cc6baa817876229e0c0668de0cd868e18f8f5d1a
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 15:29:16 +0200

    Update stopper.cpp

--- src/framework/time/stopper.cpp
+++ src/framework/time/stopper.cpp
@@ -1,3 +1,19 @@
 #include "stopper.h"
 
-namespace FW::Time {}  // namespace FW::Time
+namespace FW::Time {
+Stopper::Stopper() : running_{false} {}
+
+void Stopper::start() {}
+
+void Stopper::stop() {}
+
+void Stopper::restart() {}
+
+bool Stopper::isRunning() { return running_; }
+
+ticks_t Stopper::elapsedTimeInMicros() { return 0; }
+
+ticks_t Stopper::elapsedTimeInMillis() { return 0; }
+
+ticks_t Stopper::elapsedTimeInSeconds() { return 0; }
+}  // namespace FW::Time
