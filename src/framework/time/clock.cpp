27611170c7c42adda95ceec18f098e3cac17cc9b
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 15:29:20 +0200

    Update clock.cpp

--- src/framework/time/clock.cpp
+++ src/framework/time/clock.cpp
@@ -1,3 +1,11 @@
 #include "clock.h"
 
-namespace FW::Time {}  // namespace FW::Time
+namespace FW::Time {
+void Clock::update() { time_.resetToCurrentTime(); }
+
+ticks_t Clock::toMicros() { return time_.toMicros(); }
+
+ticks_t Clock::toMillis() { return time_.toMillis(); }
+
+ticks_t Clock::toSeconds() { return time_.toSeconds(); }
+}  // namespace FW::Time
