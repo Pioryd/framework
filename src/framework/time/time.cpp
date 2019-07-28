79b0ac14db962aed7a7e9097ad5dc8515f843313
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 15:36:42 +0200

    Update time.cpp

--- src/framework/time/time.cpp
+++ src/framework/time/time.cpp
@@ -38,4 +38,34 @@ ticks_t Time::toSeconds() { return seconds_; }
 void Time::setNull() { micros_ = millis_ = seconds_ = 0; }
 
 bool Time::isNull() { return (micros_ == 0 && millis_ == 0 && seconds_ == 0); }
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+namespace Now {
+// For std::chrono::duration_cast<> needs.
+const static auto applicationExecuteTime =
+    std::chrono::high_resolution_clock::now();
+
+std::string getTime() {
+  return to_simple_string(boost::posix_time::second_clock::universal_time());
+}
+
+ticks_t getMicros() {
+  return std::chrono::duration_cast<std::chrono::microseconds>(
+             std::chrono::high_resolution_clock::now() - applicationExecuteTime)
+      .count();
+}
+
+ticks_t getMillis() {
+  return std::chrono::duration_cast<std::chrono::milliseconds>(
+             std::chrono::high_resolution_clock::now() - applicationExecuteTime)
+      .count();
+}
+
+ticks_t getSeconds() {
+  return std::chrono::duration_cast<std::chrono::seconds>(
+             std::chrono::high_resolution_clock::now() - applicationExecuteTime)
+      .count();
+}
+}  // namespace Now
 }  // namespace FW::Time
