70a2dfb3ff453e0bab384e7aaba1ed9818ea3f6f
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 21:04:37 +0200

    Create declarations.h

---
+++ example/server/src/declarations.h
@@ -0,0 +1,9 @@
+#ifndef SERVER_DECLARATIONS_H
+#define SERVER_DECLARATIONS_H
+
+#include <framework/pch.h>
+
+namespace Server {
+class ServerApp;
+}  // namespace Server
+#endif  // #ifndef SERVER_DECLARATIONS_H
