355379c752cce39e7475aa175f9785403f5a47b0
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 18:20:55 +0200

    Update declarations.h

--- src/framework/pymodule/declarations.h
+++ src/framework/pymodule/declarations.h
@@ -3,5 +3,14 @@
 
 #include "../pch.h"
 
-namespace FW::PyModule {}  // namespace FW::PyModule
-#endif                     // #ifndef FW_PYMODULE_DECLARATIONS_H
+namespace FW::PyModule {
+struct Module;
+struct Callback;
+class Manager;
+class SignalManager;
+
+typedef std::shared_ptr<Module> Module_ptr;
+typedef std::shared_ptr<Callback> Callback_ptr;
+typedef std::shared_ptr<Manager> Manager_ptr;
+}  // namespace FW::PyModule
+#endif  // #ifndef FW_PYMODULE_DECLARATIONS_H
