8490cc66a44cd106a309f2c9435a78cfb2400ca3
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 16:19:19 +0200

    Update sqlitemanager.cpp

--- src/framework/database/sqlitemanager.cpp
+++ src/framework/database/sqlitemanager.cpp
@@ -1,3 +1,42 @@
 #include "sqlitemanager.h"
 
-namespace FW::Database {}  // namespace FW::Database
+namespace FW::Database {
+SqliteManager::SqliteManager() : SqlManager() {}
+
+SqliteManager::~SqliteManager() {}
+
+bool SqliteManager::connect(const SqlConfig& sqlConfig) { return false; }
+
+Result_sptr SqliteManager::execute(const std::string& query) {
+  return Result_sptr();
+}
+
+bool SqliteManager::beginTransaction() { return false; }
+
+bool SqliteManager::commitTransaction() { return false; }
+
+bool SqliteManager::rollbackTransaction() { return false; }
+
+std::string SqliteManager::escapeNumber(int64_t number) const { return ""; }
+
+std::string SqliteManager::escapeString(const std::string& string) const {
+  return "";
+}
+
+std::string SqliteManager::escapeBlob(const char* c_string,
+                                      uint32_t length) const {
+  return "";
+}
+
+bool SqliteManager::optimizeTables() { return false; }
+
+bool SqliteManager::triggerExists(std::string trigger) { return false; }
+
+bool SqliteManager::tableExists(std::string table) { return false; }
+
+bool SqliteManager::databaseExists() { return false; }
+
+std::string SqliteManager::getSqlClientVersion() const { return ""; }
+
+int64_t SqliteManager::getLastInsertId() const { return 0; }
+}  // namespace FW::Database
