fbc9ba908cde33f4ea9e60dc6e66186c8a9bc2b1
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 19:20:03 +0200

    Update window.cpp

--- src/framework/multimedia/window.cpp
+++ src/framework/multimedia/window.cpp
@@ -4,22 +4,61 @@
 
 namespace FW::MM {
 void Window::init(MM::SizeInt size, const std::string& name, bool fullScreen,
-                  uint32_t fpsLimit) {}
+                  uint32_t fpsLimit) {
+  window = std::make_unique<sf::RenderWindow>(
+      sf::VideoMode(size.getWidth(), size.getHeight()), name,
+      fullScreen ? sf::Style::Default | sf::Style::Fullscreen
+                 : sf::Style::Default);
 
-void Window::poll() {}
+  window->setFramerateLimit(fpsLimit);
 
-void Window::draw(const sf::Drawable& drawable) {}
+  gui = std::make_unique<tgui::Gui>(*window);
+}
 
-void Window::draw(const Color& color, const RectInt& rect) {}
+void Window::poll() {
+  sf::Event event;
+  while (window->pollEvent(event)) {
+    // "close requested" event: we close the window
+    if (event.type == sf::Event::Closed) window->close();
+
+    if (event.type == sf::Event::Resized)
+      window->setView(
+          sf::View(sf::FloatRect(0, 0, event.size.width, event.size.height)));
+
+    gui->handleEvent(event);
+  }
+}
+
+void Window::draw(const sf::Drawable& drawable) { window->draw(drawable); }
+
+void Window::draw(const Color& color, const RectInt& rect) {
+  sf::RectangleShape rectangle;
+  rectangle.setFillColor(color);
+  rectangle.setSize(rect.getSize());
+  rectangle.setPosition(rect.getTopLeft());
+  window->draw(rectangle);
+}
 
 void Window::draw(const Color& color, const RectInt& rect,
-                  const Texture_ptr texture, const RectInt& textureRect) {}
+                  const Texture_ptr texture, const RectInt& textureRect) {
+  sf::RectangleShape rectangle;
+  rectangle.setTexture(texture.get());
+  rectangle.setTextureRect(textureRect);
+  rectangle.setFillColor(color);
+  rectangle.setSize(rect.getSize());
+  rectangle.setPosition(rect.getTopLeft());
+  window->draw(rectangle);
+}
 
-void Window::clear(FW::MM::Color color) {}
+void Window::clear(FW::MM::Color color) { window->clear(color); }
 
-void Window::display() {}
+void Window::display() { window->display(); }
 
-bool Window::isOpen() { return false; }
+bool Window::isOpen() { return window->isOpen(); }
 
-SizeInt Window::getSize() { return SizeInt(); }
+SizeInt Window::getSize() {
+  // Safe cast, becouse windows size will not bigger then int32_t::max.
+  return SizeInt{static_cast<int32_t>(window->getSize().x),
+                 static_cast<int32_t>(window->getSize().y)};
+}
 }  // namespace FW::MM
