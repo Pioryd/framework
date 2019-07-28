cf2a2e4f193022307609d2b75dc6adb5051b44ee
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 15:36:36 +0200

    Update stopper.cpp

--- src/framework/time/stopper.cpp
+++ src/framework/time/stopper.cpp
@@ -3,17 +3,37 @@
 namespace FW::Time {
 Stopper::Stopper() : running_{false} {}
 
-void Stopper::start() {}
+void Stopper::start() {
+  running_ = true;
+  startTime_.resetToCurrentTime();
+  elapsedTime_.setNull();
+}
 
-void Stopper::stop() {}
+void Stopper::stop() {
+  elapsedTime_.resetToMicros(elapsedTimeInMicros());
+  startTime_.setNull();
+  running_ = false;
+}
 
-void Stopper::restart() {}
+void Stopper::restart() {
+  stop();
+  start();
+}
 
 bool Stopper::isRunning() { return running_; }
 
-ticks_t Stopper::elapsedTimeInMicros() { return 0; }
+ticks_t Stopper::elapsedTimeInMicros() {
+  if (!running_) return -1;
+  return Now::getMicros() - startTime_.toMicros() + elapsedTime_.toMicros();
+}
 
-ticks_t Stopper::elapsedTimeInMillis() { return 0; }
+ticks_t Stopper::elapsedTimeInMillis() {
+  if (!running_) return -1;
+  return Now::getMillis() - startTime_.toMillis() + elapsedTime_.toMillis();
+}
 
-ticks_t Stopper::elapsedTimeInSeconds() { return 0; }
+ticks_t Stopper::elapsedTimeInSeconds() {
+  if (!running_) return -1;
+  return Now::getSeconds() - startTime_.toSeconds() + elapsedTime_.toSeconds();
+}
 }  // namespace FW::Time
