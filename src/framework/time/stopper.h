ed6c176ba8af84120b14c6079d3d2df1c4b2b835
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 15:29:14 +0200

    Update stopper.h

--- src/framework/time/stopper.h
+++ src/framework/time/stopper.h
@@ -3,7 +3,31 @@
 
 #include "declarations.h"
 
+#include "time.h"
+
 namespace FW::Time {
-class Stopper {};
+class Stopper {
+ public:
+  Stopper();
+
+  void start();
+  void stop();
+  void restart();
+
+  bool isRunning();
+
+  // Return -1 if not running
+  ticks_t elapsedTimeInMicros();
+  // Return -1 if not running
+  ticks_t elapsedTimeInMillis();
+  // Return -1 if not running
+  ticks_t elapsedTimeInSeconds();
+
+ private:
+  Time startTime_;
+  Time elapsedTime_;
+  Time setTime_;
+  bool running_;
+};
 }  // namespace FW::Time
 #endif  // #ifndef FW_TIME_STOPPER_H
