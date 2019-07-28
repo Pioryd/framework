1bd8ab777b3df976db7fb67dc4621d5b9b34681d
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 19:15:14 +0200

    Update imagemanager.h

--- src/framework/multimedia/imagemanager.h
+++ src/framework/multimedia/imagemanager.h
@@ -2,5 +2,29 @@
 #define FW_MULTIMEDIA_IMAGEMANAGER_H
 
 #include "declarations.h"
-namespace FW::MM {}  // namespace FW::MM
-#endif               // #ifndef FW_MULTIMEDIA_IMAGEMANAGER_H
+namespace FW::MM {
+class ImageManager {
+ public:
+  struct Config {
+    bool debug_enabled = true;
+  };
+
+ public:
+  void init();
+
+  Image_ptr getImage(const std::string& imageName);
+
+  void loadImages();
+
+ protected:
+  void loadImage(const std::filesystem::path& imagePath);
+
+ public:
+  Config config;
+
+ protected:
+  std::map<std::string, Image_ptr> images_;
+  std::filesystem::path imagesPath_;
+};
+}  // namespace FW::MM
+#endif  // #ifndef FW_MULTIMEDIA_IMAGEMANAGER_H
