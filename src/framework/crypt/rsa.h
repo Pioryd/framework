dd9a07e08a7831e557ec185be95ae490db948b3d
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 15:43:25 +0200

    Update rsa.h

--- src/framework/crypt/rsa.h
+++ src/framework/crypt/rsa.h
@@ -3,4 +3,34 @@
 
 #include "declarations.h"
 
+#include <cryptopp/rsa.h>
+
+namespace FW::Crypt {
+class Rsa {
+ public:
+  // Key size must be [keySize = (msg.lenght() * 8)]
+  Rsa(std::size_t const keySize = 1024);
+  ~Rsa();
+
+  void encrypt(unsigned char *msg) const;
+  void decrypt(unsigned char *msg) const;
+
+  void generatePrivateAndPublicKey(std::stringstream &ss) const;
+
+  // Must be values in hex format with "0x" on string begin.
+  void setPublicKey(const char *n, const char *e);
+  // Must be values in hex format with "0x" on string begin.
+  void setPrivateKey(const char *n, const char *e, const char *d);
+
+ private:
+  std::string getAsString(CryptoPP::RSA::PublicKey const &publicKey) const;
+  std::string getAsString(CryptoPP::RSA::PrivateKey const &privateKey) const;
+
+ private:
+  std::size_t const keySize;
+  std::size_t const msgSize;
+  CryptoPP::RSA::PrivateKey privateKey;
+  CryptoPP::RSA::PublicKey publicKey;
+};
+}  // namespace FW::Crypt
 #endif  // #ifndef FW_CRYPT_RSA_H
