f27b532462e873c57046523b14eada133c3ab284
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 16:19:05 +0200

    Update declarations.h

--- src/framework/database/declarations.h
+++ src/framework/database/declarations.h
@@ -3,5 +3,22 @@
 
 #include "../pch.h"
 
-namespace FW::Database {}  // namespace FW::Database
-#endif                     // #ifndef FW_DATABASE_DECLARATIONS_H
+namespace FW::Database {
+class Query;
+class Result;
+class MySqlResult;
+class MariadbResult;
+class SqliteResult;
+class PostgreSqlResult;
+class SqlManager;
+class MySqlManager;
+class MariadbManager;
+class SqliteManager;
+class PostgreSqlManager;
+class MainManager;
+
+using Result_sptr = std::shared_ptr<Result>;
+using SqlManager_uptr = std::unique_ptr<SqlManager>;
+using MainManager_uptr = std::unique_ptr<MainManager>;
+}  // namespace FW::Database
+#endif  // #ifndef FW_DATABASE_DECLARATIONS_H
