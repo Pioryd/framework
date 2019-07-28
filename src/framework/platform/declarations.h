a802ae1364050b447131b989f3eef8ed97d4d902
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 14:13:42 +0200

    Create declarations.h

---
+++ src/framework/platform/declarations.h
@@ -0,0 +1,7 @@
+#ifndef FW_PLATFORM_DECLARATIONS_H
+#define FW_PLATFORM_DECLARATIONS_H
+
+#include "../pch.h"
+
+namespace FW::Platform {}  // namespace FW::Platform
+#endif                     // #ifndef FW_PLATFORM_DECLARATIONS_H
