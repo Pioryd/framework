14461defbe4e918ad5f243c2acca19c73edf6f3c
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 16:06:50 +0200

    Create sqlitemanager.h

---
+++ src/framework/database/sqlitemanager.h
@@ -0,0 +1,7 @@
+#ifndef FW_DATABASE_SQLITEMANAGER_H
+#define FW_DATABASE_SQLITEMANAGER_H
+
+#include "declarations.h"
+
+namespace FW::Database {}  // namespace FW::Database
+#endif                     // #ifndef FW_DATABASE_SQLITEMANAGER_H
