cf005d28a963c9f10864b9340223e566c074f4b2
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 19:56:46 +0200

    Update tcplistener.cpp

--- src/framework/net/tcplistener.cpp
+++ src/framework/net/tcplistener.cpp
@@ -1,3 +1,31 @@
 #include "tcplistener.h"
 
-namespace FW::Net {}  // namespace FW::Net
+namespace FW::Net {
+TcpListener::TcpListener(boost::asio::io_service& io_service,
+                         std::function<Protocol_ptr(void)> protocolOnCreate,
+                         const std::string& host, uint16_t port,
+                         Thread::EventManager& eventManager) {}
+
+TcpListener::~TcpListener() {}
+
+void TcpListener::start() {}
+
+void TcpListener::stop() {}
+
+Connection_ptr TcpListener::createConnection() { return nullptr; }
+
+void TcpListener::onConnectionClose(Connection_ptr& connection) {}
+
+void TcpListener::closeConnections() {}
+
+bool TcpListener::isRunning() { return false; }
+
+void TcpListener::asyncRestart() {}
+
+void TcpListener::asyncAccept() {}
+
+void TcpListener::onAccept(Connection_ptr connection,
+                           const boost::system::error_code& error) {}
+
+std::string TcpListener::info() { return ""; }
+}  // namespace FW::Net
