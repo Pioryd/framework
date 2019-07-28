d48ebbd2f6fe3004746507b0a9d53e12b6d696c9
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 14:19:59 +0200

    Update platform.cpp

--- src/framework/platform/android/platform.cpp
+++ src/framework/platform/android/platform.cpp
@@ -4,8 +4,18 @@
 namespace FW::Platform {
 std::string name() { return "Android"; }
 
-Time::ticks_t getFileModificationTime(std::string file) {}
+Time::ticks_t getFileModificationTime(std::string file) {
+  // TODO
+}
 
-std::wstring getExecutableDirectory() {}
+std::wstring getExecutableDirectory() {
+  char result[PATH_MAX];
+  ssize_t count = readlink("/proc/self/exe", result, PATH_MAX);
+  if (count != -1) {
+    const auto path = std::string(dirname(result));
+    return std::wstring(path.begin(), path.end());
+  }
+  return L"/";
+}
 }  // namespace FW::Platform
 #endif  // #ifdef FW_PLATFORM_LINUX
