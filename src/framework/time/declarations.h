b2f23db6ef78101b1f9465f67e8b610ed7caf1e2
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 14:10:09 +0200

    Create declarations.h

---
+++ src/framework/time/declarations.h
@@ -0,0 +1,10 @@
+#ifndef FW_TIME_DECLARATIONS_H
+#define FW_TIME_DECLARATIONS_H
+
+#include "../pch.h"
+
+namespace FW::Time {
+
+typedef int64_t ticks_t;
+}  // namespace FW::Time
+#endif  // #ifndef FW_TIME_DECLARATIONS_H
