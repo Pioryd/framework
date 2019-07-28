c53da13ae82da5a32c10eb4c645c421c93e60425
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 19:34:12 +0200

    Create tcplistener.h

---
+++ src/framework/net/tcplistener.h
@@ -0,0 +1,7 @@
+#ifndef FW_NET_TCPLISTENER_H
+#define FW_NET_TCPLISTENER_H
+
+#include "declarations.h"
+
+namespace FW::Net {}  // namespace FW::Net
+#endif                // #ifndef FW_NET_TCPLISTENER_H
