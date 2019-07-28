d734a2fc36b21c051373ffccf1a7b0a60a1e78dd
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 20:23:54 +0200

    Update sqlmanager.cpp

--- src/framework/database/sqlmanager.cpp
+++ src/framework/database/sqlmanager.cpp
@@ -1,13 +1,34 @@
 #include "sqlmanager.h"
 
+#include "../core/logger.h"
+#include "../global.h"
+
 #include <mysql/errmsg.h>
 
 namespace FW::Database {
-SqlManager::SqlManager() {}
+SqlManager::SqlManager() : transactionState_{TransactionState::NONE} {}
 
-bool SqlManager::connect(const SqlConfig& sqlConfig) {}
+bool SqlManager::connect(const SqlConfig& sqlConfig) {
+  FW_DEBUG_INSTRUCTIONS(G::Logger.debug(
+      "SQL config:"
+      "\n  Host: [" +
+      sqlConfig.host + "]\n  User: [" + sqlConfig.user + "]\n  Passwrd: [" +
+      sqlConfig.password + "]\n  Database name: [" + sqlConfig.databaseName +
+      "]\n  Port: [" + std::to_string(sqlConfig.port) + "]\n  Socket: [" +
+      sqlConfig.socket + "]\n");)
 
-Result::Result(void* handle) {}
+  sqlConfig_ = sqlConfig;
 
-size_t Result::getColumnIndex(const std::string& columnName) const { return 0; }
+  return true;
+}
+
+Result::Result(void* handle) : handle_{handle} {}
+
+size_t Result::getColumnIndex(const std::string& columnName) const {
+  auto it = fieldNames_.find(columnName);
+  if (it == fieldNames_.end())
+    throw std::runtime_error(
+        std::string("Column [" + columnName + "] does not exist.").c_str());
+  return it->second;
+}
 }  // namespace FW::Database
