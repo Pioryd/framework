0b9d757d425cafceed74b849bb4ff99d9cc992d4
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 19:34:42 +0200

    Create declarations.h

---
+++ src/framework/net/declarations.h
@@ -0,0 +1,7 @@
+#ifndef FW_NET_DECLARATIONS_H
+#define FW_NET_DECLARATIONS_H
+
+#include "../pch.h"
+
+namespace FW::Net {}  // namespace FW::Net
+#endif                // #ifndef FW_NET_DECLARATIONS_H
