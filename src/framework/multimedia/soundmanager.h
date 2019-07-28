2b289d798d0ff23548b3017ed925966bfa7c1cfb
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 19:15:02 +0200

    Update soundmanager.h

--- src/framework/multimedia/soundmanager.h
+++ src/framework/multimedia/soundmanager.h
@@ -3,5 +3,31 @@
 
 #include "declarations.h"
 
-namespace FW::MM {}  // namespace FW::MM
-#endif               // #ifndef FW_MULTIMEDIA_SOUNDMANAGER_H
+#include <SFML/Audio.hpp>
+
+namespace FW::MM {
+class SoundManager {
+ public:
+  struct Config {
+    bool debug_enabled = true;
+  };
+
+ public:
+  void init();
+
+  SoundBuffer_ptr getSoundBuffer(const std::string& soundName);
+
+  void loadSounds();
+
+ protected:
+  void loadSoundBuffer(const std::filesystem::path& soundPath);
+
+ public:
+  Config config;
+
+ protected:
+  std::map<std::string, SoundBuffer_ptr> buffers_;
+  std::filesystem::path soundsPath_;
+};
+}  // namespace FW::MM
+#endif  // #ifndef FW_MULTIMEDIA_SOUNDMANAGER_H
