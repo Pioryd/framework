5fdcc4881a7727317ef87ba1c9b650a57c7ad4b6
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 20:02:40 +0200

    Update networkmessage.cpp

--- src/framework/net/networkmessage.cpp
+++ src/framework/net/networkmessage.cpp
@@ -1,18 +1,22 @@
 #include "networkmessage.h"
 
 namespace FW::Net {
-NetworkMessage::NetworkMessage() {}
+NetworkMessage::NetworkMessage()
+    : length_{0}, position_{PACKET_SIZE_BYTES_LENGTH} {}
 
-uint16_t NetworkMessage::getLength() const { return 0; }
-void NetworkMessage::setLength(uint16_t length) {}
+uint16_t NetworkMessage::getLength() const { return length_; }
+void NetworkMessage::setLength(uint16_t length) { length_ = length; }
 
-uint8_t* NetworkMessage::getBuffer() { return 0; }
+uint8_t* NetworkMessage::getBuffer() { return buffer_; }
 
-const uint8_t* NetworkMessage::getBuffer() const { return 0; }
+const uint8_t* NetworkMessage::getBuffer() const { return buffer_; }
 
-uint8_t* NetworkMessage::getBufferAtCurrentPosition() { return 0; }
+uint8_t* NetworkMessage::getBufferAtCurrentPosition() {
+  return buffer_ + position_;
+}
+const uint8_t* NetworkMessage::getBufferAtCurrentPosition() const {
+  return buffer_ + position_;
+}
 
-const uint8_t* NetworkMessage::getBufferAtCurrentPosition() const { return 0; }
-
-uint16_t NetworkMessage::getBufferPosition() const { return 0; }
+uint16_t NetworkMessage::getBufferPosition() const { return position_; }
 }  // namespace FW::Net
