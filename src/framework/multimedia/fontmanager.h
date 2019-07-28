885d13c8c04a842a0642d337fb1cfab91d23e55b
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 19:15:30 +0200

    Update fontmanager.h

--- src/framework/multimedia/fontmanager.h
+++ src/framework/multimedia/fontmanager.h
@@ -3,5 +3,29 @@
 
 #include "declarations.h"
 
-namespace FW::MM {}  // namespace FW::MM
-#endif               // #ifndef FW_MULTIMEDIA_FONTMANAGER_H
+namespace FW::MM {
+class FontManager {
+ public:
+  struct Config {
+    bool debug_enabled = true;
+  };
+
+ public:
+  void init();
+
+  Font_ptr getFont(const std::string& fontName);
+
+  void loadFonts();
+
+ protected:
+  void loadFont(const std::filesystem::path& fontPath);
+
+ public:
+  Config config;
+
+ protected:
+  std::map<std::string, Font_ptr> fonts_;
+  std::filesystem::path fontsPath_;
+};
+}  // namespace FW::MM
+#endif  // #ifndef FW_MULTIMEDIA_FONTMANAGER_H
