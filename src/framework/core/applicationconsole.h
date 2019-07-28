9b9e74db923295f7cd1058db85517a07ed29fde9
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 20:24:02 +0200

    Update applicationconsole.h

--- src/framework/core/applicationconsole.h
+++ src/framework/core/applicationconsole.h
@@ -6,6 +6,19 @@
 #include "applicationbase.h"
 
 namespace FW::Core {
-class ApplicationConsole : public ApplicationBase {};
+class ApplicationConsole : public ApplicationBase {
+ public:
+  ApplicationConsole();
+  ~ApplicationConsole();
+
+ protected:
+  void startMainLoop() override;
+
+ protected:
+  void onInit(const std::vector<std::string>&,
+              const std::string& title) override;
+  void onTerminate() override;
+  void onJoin() override;
+};
 }  // namespace FW::Core
 #endif  // #ifndef FW_CORE_APPLICATION_CONSOLE_H
