2dc19901eb31f04dc33a729734de7995e602f56b
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 20:24:13 +0200

    Create bindmodules.cpp

---
+++ src/framework/bindmodules.cpp
@@ -0,0 +1,52 @@
+//#include "global.h"
+
+#include "global.h"
+#include "pymodule/manager.h"
+#include "core/logger.h"
+
+#include <pybind11/embed.h>
+
+PYBIND11_EMBEDDED_MODULE(fw_module_manager, m) {
+  // `m` is a `pybind11::module` which is used to bind functions and classes
+  m.def("connect",
+        [](const std::string& signal, pybind11::object function,
+           pybind11::object* globalVariable = nullptr) {
+          FW::G::PyModuleManager->connect(signal, function, globalVariable);
+        },
+        "Connect to signal", pybind11::arg("signal"), pybind11::arg("object"),
+        pybind11::arg("globalVariable") = (pybind11::object*)nullptr),
+
+      m.def("disconnect",
+            [](const std::string& signal, pybind11::object function,
+               pybind11::object* globalVariable = nullptr) {
+              FW::G::PyModuleManager->disconnect(signal, function, globalVariable);
+            },
+            "Disconnect to signal", pybind11::arg("signal"), pybind11::arg("object"),
+            pybind11::arg("globalVariable") = (pybind11::object*)nullptr);
+}
+PYBIND11_EMBEDDED_MODULE(fw_core_logger, m) {
+  // `m` is a `pybind11::module` which is used to bind functions and classes
+  m.def("info",
+        [](const std::string& message, const std::string& function = "",
+           const std::string& moduleGlobalName = "") {
+          FW::G::Logger.info(message, function, moduleGlobalName);
+        },
+        "Print info log", pybind11::arg("message"), pybind11::arg("function") = "",
+        pybind11::arg("moduleGlobalName") = ""),
+
+      m.def("error",
+            [](const std::string& message, const std::string& function = "",
+               const std::string& moduleGlobalName = "") {
+              FW::G::Logger.info(message, function, moduleGlobalName);
+            },
+            "Print error log", pybind11::arg("message"), pybind11::arg("function") = "",
+            pybind11::arg("moduleGlobalName") = ""),
+
+      m.def("debug",
+            [](const std::string& message, const std::string& function = "",
+               const std::string& moduleGlobalName = "") {
+              FW::G::Logger.debug(message, function, moduleGlobalName);
+            },
+            "Print debug log", pybind11::arg("message"), pybind11::arg("function") = "",
+            pybind11::arg("moduleGlobalName") = "");
+}
