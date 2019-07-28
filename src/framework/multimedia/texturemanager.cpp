3d8ff8adf53f64f875933b2a3e5d4a9e60b47065
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 19:15:00 +0200

    Update texturemanager.cpp

--- src/framework/multimedia/texturemanager.cpp
+++ src/framework/multimedia/texturemanager.cpp
@@ -1,3 +1,13 @@
 #include "texturemanager.h"
 
-namespace FW::MM {}  // namespace FW::MM
+namespace FW::MM {
+void TextureManager::init() {}
+
+Texture_ptr TextureManager::getTexture(const std::string& textureName) {
+  return nullptr;
+}
+
+void TextureManager::loadTextures() {}
+
+void TextureManager::loadTexture(const std::filesystem::path& texturePath) {}
+}  // namespace FW::MM
