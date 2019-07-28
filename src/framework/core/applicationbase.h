2f0b35e7b7a77799436d7eabd769a3ba759372a9
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 16:45:08 +0200

    Create applicationbase.h

---
+++ src/framework/core/applicationbase.h
@@ -0,0 +1,11 @@
+#ifndef FW_CORE_APPLICATIONBASE_H
+#define FW_CORE_APPLICATIONBASE_H
+
+#include "declarations.h"
+
+namespace FW::Core {
+class ApplicationBase {
+  std::wstring getWorkingDirectory();
+};
+}  // namespace FW::Core
+#endif  // #ifndef FW_CORE_APPLICATIONBASE_H
