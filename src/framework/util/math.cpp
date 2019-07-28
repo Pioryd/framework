a04acf7b5d1d8673a3f6350e5a27be44db99c6cf
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 14:27:05 +0200

    Update math.cpp

--- src/framework/util/math.cpp
+++ src/framework/util/math.cpp
@@ -1,3 +1,15 @@
 #include "math.h"
 
-namespace FW::Util {}  // namespace FW::Util
+namespace FW::Util {
+uint32_t Math::adler32(const uint8_t* data, size_t lenght) {
+  // TODO
+}
+
+long Math::random_range(long min, long max) {
+  // TODO
+}
+
+float Math::random_range(float min, float max) {
+  // TODO
+}
+}  // namespace FW::Util
