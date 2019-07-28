2dfd47842a5263f2a1e76e4df6dbaaeca7901472
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 18:29:14 +0200

    Create window.h

---
+++ src/framework/multimedia/window.h
@@ -0,0 +1,7 @@
+#ifndef FW_MULTIMEDIA_WINDOW_H
+#define FW_MULTIMEDIA_WINDOW_H
+
+#include "declarations.h"
+
+namespace FW::MM {}  // namespace FW::MM
+#endif               // #ifndef FW_MULTIMEDIA_WINDOW_H
