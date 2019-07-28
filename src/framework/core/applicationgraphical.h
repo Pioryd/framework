240fa1db4c0181939fb633914a2d7583edcf9677
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 20:23:58 +0200

    Update applicationgraphical.h

--- src/framework/core/applicationgraphical.h
+++ src/framework/core/applicationgraphical.h
@@ -6,6 +6,19 @@
 #include "applicationbase.h"
 
 namespace FW::Core {
-class ApplicationGraphical : public ApplicationBase {};
+class ApplicationGraphical : public ApplicationBase {
+ public:
+  ApplicationGraphical();
+  ~ApplicationGraphical();
+
+ protected:
+  void startMainLoop() override;
+
+ protected:
+  void onInit(const std::vector<std::string>&,
+              const std::string& title) override;
+  void onTerminate() override;
+  void onJoin() override;
+};
 }  // namespace FW::Core
 #endif  // #ifndef FW_CORE_APPLICATION_GRAPHICAL_H
