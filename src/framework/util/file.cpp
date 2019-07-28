24bd99bb9c2249665f2efe9f287332bbed3c9f77
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 14:11:12 +0200

    Create file.cpp

---
+++ src/framework/util/file.cpp
@@ -0,0 +1,12 @@
+#include "file.h"
+
+#include <cstring>
+
+namespace FW::IO {
+Time::ticks_t File::getFileModificationTime(const std::string& fileName) {
+  // TODO
+}
+std::wstring File::getExecutableDirectory() {
+  // TODO
+}
+}  // namespace FW::IO
