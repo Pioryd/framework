a41d8b8742a65d526afd59accf49e412cc9584ce
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 20:23:45 +0200

    Update global.h

--- src/framework/global.h
+++ src/framework/global.h
@@ -1,14 +1,47 @@
 #ifndef FW_GLOBAL_H
 #define FW_GLOBAL_H
 
+#include <boost/current_function.hpp>
 #include "core/declarations.h"
+#include "crypt/declarations.h"
+#include "platform/declarations.h"
+#include "pymodule/declarations.h"
 #include "thread/declarations.h"
 #include "thread/lookafter.h"
+#include "time/declarations.h"
 
-// Globals used by framework and project.
+#ifdef FW_DEF_GRAPHICAL_APPLICATION
+#include "multimedia/declarations.h"
+#endif
+
+// To use below macros class must inherith from class TraceInfo
+#define FW_G_LOGGER_DEBUG_TRACE(message) \
+  FW::G::Logger.debug(message, BOOST_CURRENT_FUNCTION, traceInfo)
+#define FW_G_LOGGER_INFO_TRACE(message) \
+  FW::G::Logger.info(message, BOOST_CURRENT_FUNCTION, traceInfo)
+#define FW_G_LOGGER_WARNING_TRACE(message) \
+  FW::G::Logger.warning(message, BOOST_CURRENT_FUNCTION, traceInfo)
+#define FW_G_LOGGER_ERROR_TRACE(message) \
+  FW::G::Logger.error(message, BOOST_CURRENT_FUNCTION, traceInfo)
+#define FW_G_LOGGER_FATAL_TRACE(message) \
+  FW::G::Logger.fatal(message, BOOST_CURRENT_FUNCTION, traceInfo)
+
+// Globals used by freamwork and project.
 namespace FW::G {
 extern FW::Core::Logger Logger;
+extern FW::Time::Clock Clock;
+extern FW::Crypt::Rsa Rsa;
 extern FW::Thread::EventManager eventManager;
+
+extern FW::Thread::LookAfter<FW::PyModule::Manager> PyModuleManager;
 extern FW::Thread::LookAfter<FW::Core::Application> Application;
+
+#ifdef FW_DEF_GRAPHICAL_APPLICATION
+extern FW::MM::SoundManager SoundManager;
+extern FW::MM::FontManager FontManager;
+extern FW::MM::ImageManager ImageManager;
+extern FW::MM::TextureManager TextureManager;
+extern FW::MM::Window Window;
+#endif
 }  // namespace FW::G
 #endif  // #ifndef FW_GLOBAL_H
