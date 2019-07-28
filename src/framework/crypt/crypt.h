64a93b4895c2b40f7dca39c0033afc508dddbbdb
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 15:43:31 +0200

    Update crypt.h

--- src/framework/crypt/crypt.h
+++ src/framework/crypt/crypt.h
@@ -3,5 +3,24 @@
 
 #include "declarations.h"
 
-namespace FW::Crypt {}  // namespace FW::Crypt
-#endif                  // #ifndef FW_CRYPT_H
+namespace FW::Crypt {
+namespace Xtea {
+using Key = std::array<uint32_t, 4>;
+bool encrypt(uint8_t* buffer, size_t size, const Key& key);
+bool encrypt(uint16_t* buffer, size_t size, const Key& key);
+bool encrypt(uint32_t* buffer, size_t size, const Key& key);
+bool decrypt(uint8_t* buffer, size_t size, const Key& key);
+bool decrypt(uint16_t* buffer, size_t size, const Key& key);
+bool decrypt(uint32_t* buffer, size_t size, const Key& key);
+}  // namespace Xtea
+namespace Sha1 {
+std::string hash(const std::string& message);
+}  // namespace Sha1
+namespace Sha256 {
+std::string hash(const std::string& message);
+}  // namespace Sha256
+namespace Sha512 {
+std::string hash(const std::string& message);
+}  // namespace Sha512
+}  // namespace FW::Crypt
+#endif  // #ifndef FW_CRYPT_H
