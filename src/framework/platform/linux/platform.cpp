757c5757a4ac05609d1a60e059095439ad072ee6
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 14:20:02 +0200

    Update platform.cpp

--- src/framework/platform/linux/platform.cpp
+++ src/framework/platform/linux/platform.cpp
@@ -1,9 +1,31 @@
 #include "../platform.h"
 
+#include <libgen.h>
+#include <linux/limits.h>
+#include <unistd.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+
 namespace FW::Platform {
 std::string name() { return "Linux"; }
 
-Time::ticks_t getFileModificationTime(std::string file) {}
+Time::ticks_t getFileModificationTime(std::string file) {
+  struct stat attrib;
+  if (stat(file.c_str(), &attrib) == 0) return attrib.st_mtime;
+  return 0;
+}
 
-std::wstring getExecutableDirectory() {}
+std::wstring getExecutableDirectory() {
+  char result[PATH_MAX];
+  ssize_t count = readlink("/proc/self/exe", result, PATH_MAX);
+  if (count != -1) {
+    const auto path = std::string(dirname(result));
+    return std::wstring(path.begin(), path.end());
+  }
+  return L"/";
+}
 }  // namespace FW::Platform
