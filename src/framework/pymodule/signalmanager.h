c27ae2fb61ba1c3114ed3fe1cda492f220313ba4
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 17:47:20 +0200

    Create signalmanager.h

---
+++ src/framework/pymodule/signalmanager.h
@@ -0,0 +1,7 @@
+#ifndef FW_PYMODULE_SIGNALMANAGER_H
+#define FW_PYMODULE_SIGNALMANAGER_H
+
+#include "declarations.h"
+
+namespace FW::PyModule {}  // namespace FW::PyModule
+#endif                     // #ifndef FW_PYMODULE_SIGNALMANAGER_H
