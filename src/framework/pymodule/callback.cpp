57c08c18fe8917711e2ad761ff8ee9b0b97b881c
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 18:20:53 +0200

    Update callback.cpp

--- src/framework/pymodule/callback.cpp
+++ src/framework/pymodule/callback.cpp
@@ -1,6 +1,24 @@
 #include "callback.h"
 
-#include <boost/algorithm/string.hpp>
-#include <boost/algorithm/string/replace.hpp>
+namespace FW::PyModule {
+Callback::Callback(
+    Module_ptr topLevelModule,
+    std::shared_ptr<std::vector<std::string>> splittedFunctionGlobalName,
+    bool callFromGlobalVariable) {}
 
-namespace FW::PyModule {}  // namespace FW::PyModule
+bool Callback::operator==(const Callback& object) const { return false; }
+
+bool Callback::operator!=(const Callback& object) const { return false; }
+
+Module_ptr Callback::getTopLevelModule() const { return Module_ptr; }
+
+std::string Callback::getModuleTopLevelName() const { return ""; }
+
+std::string Callback::getFunctionName() const { return ""; }
+
+std::string Callback::getFunctionGlobalName() const { return ""; }
+
+std::string Callback::getGlobalVariableName() const { return ""; }
+
+bool Callback::isCallFromGlobalVariable() const { return false; }
+}  // namespace FW::PyModule
