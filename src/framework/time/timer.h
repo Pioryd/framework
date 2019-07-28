bc7e9bb511e36e34c45a3f73a556ae94022aa555
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 15:29:07 +0200

    Update timer.h

--- src/framework/time/timer.h
+++ src/framework/time/timer.h
@@ -4,6 +4,35 @@
 #include "declarations.h"
 
 namespace FW::Time {
-class Timer {};
+class Timer {
+ public:
+  Timer();
+
+  void start();
+  void stop();
+  void reset();
+
+  bool isRunning();
+
+  // Remaining time is reset after set
+  void setCountdownInMicros(ticks_t micros);
+  // Remaining time is reset after set
+  void setCountdownInMillis(ticks_t milis);
+  // Remaining time is reset after set
+  void setCountdownInSecond(ticks_t seconds);
+
+  // Return -1 if not running. Return 0 if the countdown has been completed.
+  ticks_t remainingTimeInMicros();
+  // Return -1 if not running. Return 0 if the countdown has been completed
+  ticks_t remainingTimeInMillis();
+  // Return -1 if not running. Return 0 if the countdown has been completed
+  ticks_t remainingTimeInSeconds();
+
+ private:
+  Time startTime_;
+  Time remainingTime_;
+  Time setTime_;
+  bool running_;
+};
 }  // namespace FW::Time
 #endif  // #ifndef FW_TIME_TIMER_H
