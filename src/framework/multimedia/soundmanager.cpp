972fd3d1822b6ca185a41404f346276fe6840473
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 19:15:03 +0200

    Update soundmanager.cpp

--- src/framework/multimedia/soundmanager.cpp
+++ src/framework/multimedia/soundmanager.cpp
@@ -1,3 +1,13 @@
 #include "soundmanager.h"
 
-namespace FW::MM {}  // namespace FW::MM
+namespace FW::MM {
+void SoundManager::init() {}
+
+SoundBuffer_ptr SoundManager::getSoundBuffer(const std::string& soundName) {
+  return nullptr;
+}
+
+void SoundManager::loadSounds() {}
+
+void SoundManager::loadSoundBuffer(const std::filesystem::path& soundPath) {}
+}  // namespace FW::MM
