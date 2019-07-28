395e422943ef965fa9756f6c808e056722d81515
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 19:56:43 +0200

    Update declarations.h

--- src/framework/net/declarations.h
+++ src/framework/net/declarations.h
@@ -3,5 +3,20 @@
 
 #include "../pch.h"
 
-namespace FW::Net {}  // namespace FW::Net
-#endif                // #ifndef FW_NET_DECLARATIONS_H
+namespace FW::Net {
+class Protocol;
+class OutputMessage;
+class InputMessage;
+class Connection;
+class TcpListener;
+class TcpClient;
+
+typedef std::shared_ptr<Protocol> Protocol_ptr;
+typedef std::shared_ptr<OutputMessage> OutputMessage_ptr;
+typedef std::shared_ptr<InputMessage> InputMessage_ptr;
+typedef std::shared_ptr<Connection> Connection_ptr;
+typedef std::weak_ptr<Connection> Connection_wptr;
+typedef std::shared_ptr<TcpListener> TcpListener_ptr;
+typedef std::shared_ptr<TcpClient> TcpClient_ptr;
+}  // namespace FW::Net
+#endif  // #ifndef FW_NET_DECLARATIONS_H
