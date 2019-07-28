799f62e28f7cb0af57a3911ebfde1a6022374dc8
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 16:33:31 +0200

    Update sqlmanager.cpp

--- src/framework/database/sqlmanager.cpp
+++ src/framework/database/sqlmanager.cpp
@@ -6,4 +6,8 @@ namespace FW::Database {
 SqlManager::SqlManager() {}
 
 bool SqlManager::connect(const SqlConfig& sqlConfig) {}
+
+Result::Result(void* handle) {}
+
+size_t Result::getColumnIndex(const std::string& columnName) const { return 0; }
 }  // namespace FW::Database
