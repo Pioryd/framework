409fde86ffec20020b31db2e454e480bc9a4ed29
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 16:55:09 +0200

    Create event.h

---
+++ src/framework/thread/event.h
@@ -0,0 +1,9 @@
+#ifndef FW_THREAD_EVENT_H
+#define FW_THREAD_EVENT_H
+
+#include "declarations.h"
+
+namespace FW::Thread {
+class Event {};
+}  // namespace FW::Thread
+#endif  // #ifndef FW_THREAD_EVENT_H
