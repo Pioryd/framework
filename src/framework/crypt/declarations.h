091c4ec6f09493b4fba76c0fe12ed58f66ce77c7
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 15:40:17 +0200

    Create declarations.h

---
+++ src/framework/crypt/declarations.h
@@ -0,0 +1,7 @@
+#ifndef FW_CRYPT_DECLARATIONS_H
+#define FW_CRYPT_DECLARATIONS_H
+
+#include "../pch.h"
+
+namespace FW::Crypt {}  // namespace FW::Crypt
+#endif                  // #ifndef FW_CRYPT_DECLARATIONS_H
