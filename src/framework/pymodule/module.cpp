91cea2becc9a58b2623b4b4f4df1b7486be89dce
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 18:20:45 +0200

    Update module.cpp

--- src/framework/pymodule/module.cpp
+++ src/framework/pymodule/module.cpp
@@ -1,3 +1,14 @@
 #include "module.h"
 
-namespace FW::PyModule {}  // namespace FW::PyModule
+namespace FW::PyModule {
+Module::Module(const std::string& fileNameWithoutExtension,
+               const std::string& pathWithoutExtension, bool isPackage) {}
+
+std::string Module::getName() const { return ""; }
+
+std::string Module::getPathWithoutExtension() const { return ""; }
+
+pybind11::module Module::getObject() const { return pybind11::module; }
+
+void Module::setObject(const pybind11::module& newObject) {}
+}  // namespace FW::PyModule
