17ebb11d4a68e9914d239f144245c3b6acd937f9
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 16:06:44 +0200

    Create sqlmanager.h

---
+++ src/framework/database/sqlmanager.h
@@ -0,0 +1,7 @@
+#ifndef FW_DATABASE_SQLMANAGER_H
+#define FW_DATABASE_SQLMANAGER_H
+
+#include "declarations.h"
+
+namespace FW::Database {}  // namespace FW::Database
+#endif                     // #ifndef FW_DATABASE_SQLMANAGER_H
