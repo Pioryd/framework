ae15d7e46a16b8c67b5dbf3c660e9a8742a2c612
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 16:55:02 +0200

    Create eventmanager.h

---
+++ src/framework/thread/eventmanager.h
@@ -0,0 +1,9 @@
+#ifndef FRAMEWORK_THREAD_EVENTMANAGER_H
+#define FRAMEWORK_THREAD_EVENTMANAGER_H
+
+#include "eventpoll.h"
+
+namespace FW::Thread {
+class EventManager {};
+}  // namespace FW::Thread
+#endif  // #ifndef FRAMEWORK_THREAD_EVENTMANAGER_H
