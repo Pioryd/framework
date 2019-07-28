1510a9bd1a498ee7fbba79f1c33f853f50fa217a
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 18:29:31 +0200

    Create imagemanager.h

---
+++ src/framework/multimedia/imagemanager.h
@@ -0,0 +1,6 @@
+#ifndef FW_MULTIMEDIA_IMAGEMANAGER_H
+#define FW_MULTIMEDIA_IMAGEMANAGER_H
+
+#include "declarations.h"
+namespace FW::MM {}  // namespace FW::MM
+#endif               // #ifndef FW_MULTIMEDIA_IMAGEMANAGER_H
