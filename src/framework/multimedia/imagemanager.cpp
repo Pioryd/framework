9251c68d538033fd3de4affc46302a06bbb6ff9b
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 19:15:28 +0200

    Update imagemanager.cpp

--- src/framework/multimedia/imagemanager.cpp
+++ src/framework/multimedia/imagemanager.cpp
@@ -1,3 +1,13 @@
 #include "imagemanager.h"
 
-namespace FW::MM {}  // namespace FW::MM
+namespace FW::MM {
+void ImageManager::init() {}
+
+Image_ptr ImageManager::getImage(const std::string& imageName) {
+  return nullptr;
+}
+
+void ImageManager::loadImages() {}
+
+void ImageManager::loadImage(const std::filesystem::path& imagePath) {}
+}  // namespace FW::MM
