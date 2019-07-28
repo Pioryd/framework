3f478d2bca58d99e0d8aa9c17be974b9a83fa1c6
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 21:00:45 +0200

    Create signal.py

---
+++ example/modules/signal.py
@@ -0,0 +1,22 @@
+import fw_core_logger
+
+def onLoad():
+  fw_core_logger.info("Moduele: v5 [%s] called [onLoad()]" % (__name__))
+  
+def onInit():
+  fw_core_logger.info("Moduele: v5 [%s] called [onInit()]" % (__name__))
+
+def onTerminate():
+  fw_core_logger.info("Moduele: [%s] called [onTerminate()]" % (__name__))
+
+def onReload(reloadedLib):
+  fw_core_logger.info("Moduele: [%s] called [onReload()]" % (__name__))
+  fw_core_logger.info("Reloaded lib: [%s]." % reloadedLib)
+  #importlib.reload(config.tools.reader)
+
+# core
+onInit = "onInit"
+onTerminate = "onTerminate"
+onReload = "onReload"
+# test needs
+onKeyDown = "onKeyDown"
