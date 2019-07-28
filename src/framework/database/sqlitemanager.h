6e79e5936e6dc2f4e0debbf3f4bc4faf814e9965
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 16:19:17 +0200

    Update sqlitemanager.h

--- src/framework/database/sqlitemanager.h
+++ src/framework/database/sqlitemanager.h
@@ -3,5 +3,43 @@
 
 #include "declarations.h"
 
-namespace FW::Database {}  // namespace FW::Database
-#endif                     // #ifndef FW_DATABASE_SQLITEMANAGER_H
+#include "sqlmanager.h"
+
+#include <sqlite3.h>
+
+namespace FW::Database {
+class SqliteManager : public SqlManager {
+ public:
+  SqliteManager();
+  virtual ~SqliteManager();
+
+  SqliteManager(const SqliteManager&) = delete;
+  SqliteManager& operator=(const SqliteManager&) = delete;
+
+  bool connect(const SqlConfig& sqlConfig) override;
+
+  Result_sptr execute(const std::string& query) override;
+
+  bool beginTransaction() override;
+  bool commitTransaction() override;
+  bool rollbackTransaction() override;
+
+  std::string escapeNumber(int64_t number) const override;
+  std::string escapeString(const std::string& string) const override;
+  std::string escapeBlob(const char* blob, uint32_t length) const override;
+
+  bool optimizeTables() override;
+  bool triggerExists(std::string trigger) override;
+  bool tableExists(std::string table) override;
+  bool databaseExists() override;
+
+  std::string getSqlClientVersion() const override;
+  int64_t getLastInsertId() const override;
+
+ protected:
+  sqlite3* handle_ = nullptr;
+};
+
+class SqliteResult : public Result {};
+}  // namespace FW::Database
+#endif  // #ifndef FW_DATABASE_SQLITEMANAGER_H
