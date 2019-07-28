103c37b42e0425003efa813672ffc5a9949b1fa3
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 15:43:34 +0200

    Update crypt.cpp

--- src/framework/crypt/crypt.cpp
+++ src/framework/crypt/crypt.cpp
@@ -1,3 +1,45 @@
 #include "crypt.h"
 
-namespace FW::Crypt {}  // namespace FW::Crypt
+#include <cryptopp/files.h>
+#include <cryptopp/filters.h>
+#include <cryptopp/hex.h>
+#include <cryptopp/sha.h>
+
+namespace FW::Crypt {
+
+bool Xtea::encrypt(uint8_t* buffer, size_t size, const Key& key) {
+  return false;
+}
+
+bool Xtea::encrypt(uint16_t* buffer, size_t size, const Key& key) {
+  return false;
+}
+
+bool Xtea::encrypt(uint32_t* buffer, size_t size, const Key& key) {
+  return false;
+}
+
+bool Xtea::decrypt(uint8_t* buffer, size_t size, const Key& key) {
+  return false;
+}
+
+bool Xtea::decrypt(uint16_t* buffer, size_t size, const Key& key) {
+  return false;
+}
+
+bool Xtea::decrypt(uint32_t* buffer, size_t size, const Key& key) {
+  return false;
+}
+
+std::string Sha1::hash(const std::string& message) {
+  return "";
+}
+
+std::string Sha256::hash(const std::string& message) {
+  return "";
+}
+
+std::string Sha512::hash(const std::string& message) {
+  return "";
+}
+}  // namespace FW::Crypt
