f63fc3917d06600eb29ef56311828ad7df593555
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 16:46:20 +0200

    Create declarations.h

---
+++ src/framework/thread/declarations.h
@@ -0,0 +1,7 @@
+#ifndef FW_THREAD_DECLARATIONS_H
+#define FW_THREAD_DECLARATIONS_H
+
+#include "../pch.h"
+
+namespace FW::Thread {}  // namespace FW::Thread
+#endif                   // #ifndef FW_THREAD_DECLARATIONS_H
