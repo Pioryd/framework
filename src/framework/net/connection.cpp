d6a3cc6ccc41f2e2c466bd7d1515e9fc3bd4fe03
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 19:57:11 +0200

    Update connection.cpp

--- src/framework/net/connection.cpp
+++ src/framework/net/connection.cpp
@@ -1,3 +1,52 @@
 #include "connection.h"
 
-namespace FW::Net {}  // namespace FW::Net
+#include "../core/logger.h"
+#include "../global.h"
+
+#include "protocol.h"
+#include "tcplistener.h"
+
+namespace FW::Net {
+Connection::Connection(boost::asio::io_service& io_service,
+                       std::function<void(Connection_ptr&)> onClose,
+                       const Protocol_ptr& protocol,
+                       uint32_t packetsPerSecond) {}
+
+Connection::~Connection() {}
+
+void Connection::connect(
+    boost::asio::ip::basic_resolver<boost::asio::ip::tcp>::iterator
+        endpointIterator) {}
+
+void Connection::close() {}
+
+void Connection::send(const OutputMessage_ptr& msg) {}
+
+unsigned long Connection::getHost() { return 0; }
+
+std::string Connection::getHostAsString() { return ""; }
+
+unsigned short Connection::getPort() { return 0; }
+
+std::string Connection::getPortAsString() { return ""; }
+
+std::string Connection::getInfo() { return ""; }
+
+void Connection::readPacket() {}
+
+void Connection::readPacketSize(const boost::system::error_code& error) {}
+
+void Connection::readPacketBody(const boost::system::error_code& error) {}
+
+void Connection::writePacket(const OutputMessage_ptr& msg) {}
+
+void Connection::onConnect(const boost::system::error_code& error) {}
+
+void Connection::onAccept() {}
+
+void Connection::onWritePacket(const boost::system::error_code& error) {}
+
+void Connection::onTimeout(const boost::system::error_code& error) {}
+
+boost::asio::ip::tcp::socket& Connection::getSocket() { return nullptr; }
+}  // namespace FW::Net
