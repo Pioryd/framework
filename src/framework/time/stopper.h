cdf0e97ba924477be2e5be9f4f38c64b50bd6096
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 15:05:30 +0200

    Create stopper.h

---
+++ src/framework/time/stopper.h
@@ -0,0 +1,9 @@
+#ifndef FW_TIME_STOPPER_H
+#define FW_TIME_STOPPER_H
+
+#include "declarations.h"
+
+namespace FW::Time {
+class Stopper {};
+}  // namespace FW::Time
+#endif  // #ifndef FW_TIME_STOPPER_H
