e091e9a2a000a30f2d4d8a7a56e941de991052bc
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 16:06:39 +0200

    Create declarations.h

---
+++ src/framework/database/declarations.h
@@ -0,0 +1,7 @@
+#ifndef FW_DATABASE_DECLARATIONS_H
+#define FW_DATABASE_DECLARATIONS_H
+
+#include "../pch.h"
+
+namespace FW::Database {}  // namespace FW::Database
+#endif                     // #ifndef FW_DATABASE_DECLARATIONS_H
