91c4e6c8956d6d3431f97bd752913e2f3de71207
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 21:03:59 +0200

    Create global.h

---
+++ example/client/src/global.h
@@ -0,0 +1,13 @@
+#ifndef FW_CLIENT_GLOBAL_H
+#define FW_CLIENT_GLOBAL_H
+
+#include "declarations.h"
+
+#include <framework/database/declarations.h>
+
+namespace Client {
+namespace G {
+extern ClientApp clientApp;
+}  // namespace G
+}  // namespace Client
+#endif  // #ifndef FW_CLIENT_GLOBAL_H
