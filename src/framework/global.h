0a920dab1c861b0e5e2fc450d2b3bc0cad01ab3c
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 17:32:56 +0200

    Update global.h

--- src/framework/global.h
+++ src/framework/global.h
@@ -2,11 +2,13 @@
 #define FW_GLOBAL_H
 
 #include "core/declarations.h"
+#include "thread/declarations.h"
 #include "thread/lookafter.h"
 
 // Globals used by framework and project.
 namespace FW::G {
 extern FW::Core::Logger Logger;
+extern FW::Thread::EventManager eventManager;
 extern FW::Thread::LookAfter<FW::Core::Application> Application;
 }  // namespace FW::G
 #endif  // #ifndef FW_GLOBAL_H
