0da1e7e7feaea0765b925b1186adbdce07e9aea7
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 18:29:40 +0200

    Create declarations.h

---
+++ src/framework/multimedia/declarations.h
@@ -0,0 +1,7 @@
+#ifndef FW_MULTIMEDIA_DECLARATIONS_H
+#define FW_MULTIMEDIA_DECLARATIONS_H
+
+#include "../pch.h"
+
+namespace FW::MM {}  // namespace FW::MM
+#endif               // #ifndef FW_MULTIMEDIA_DECLARATIONS_H
