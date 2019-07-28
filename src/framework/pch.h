663f7fc41512929abc0cb4b203b475940293d642
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 14:10:04 +0200

    Update pch.h

--- src/framework/pch.h
+++ src/framework/pch.h
@@ -1,4 +1,31 @@
 #ifndef FW_PCH_H
 #define FW_PCH_H
 
+#include <filesystem>
+#include <fstream>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <set>
+#include <sstream>
+
+#include <assert.h>
+#include <condition_variable>
+#include <exception>
+#include <functional>
+#include <memory>
+#include <thread>
+
+#include <array>
+#include <deque>
+#include <list>
+#include <map>
+#include <unordered_map>
+
+#include <string>
+#include <vector>
+
+#include <chrono>
+#include <cstdio>
+
 #endif  // #ifndef FW_PCH_H
