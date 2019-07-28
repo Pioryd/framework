1a8550424623b07dd5c35d8bc2f6c918f8bdfb10
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 15:53:17 +0200

    Create global.h

---
+++ src/framework/global.h
@@ -0,0 +1,10 @@
+#ifndef FW_GLOBAL_H
+#define FW_GLOBAL_H
+
+#include "core/declarations.h"
+
+// Globals used by framework and project.
+namespace FW::G {
+extern FW::Core::Logger Logger;
+}  // namespace FW::G
+#endif  // #ifndef FW_GLOBAL_H
