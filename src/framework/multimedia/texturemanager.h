fd55be02239898f97fe9ec6143628d83f449b73a
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 19:14:59 +0200

    Update texturemanager.h

--- src/framework/multimedia/texturemanager.h
+++ src/framework/multimedia/texturemanager.h
@@ -3,5 +3,31 @@
 
 #include "declarations.h"
 
-namespace FW::MM {}  // namespace FW::MM
-#endif               // #ifndef FW_MULTIMEDIA_TEXTUREMANAGER_H
+#include "../thread/eventmanager.h"
+
+namespace FW::MM {
+class TextureManager {
+ public:
+  struct Config {
+    bool debug_enabled = true;
+  };
+
+ public:
+  void init();
+
+  Texture_ptr getTexture(const std::string& textureName);
+
+  void loadTextures();
+
+ protected:
+  void loadTexture(const std::filesystem::path& texturePath);
+
+ public:
+  Config config;
+
+ protected:
+  std::map<std::string, Texture_ptr> textures_;
+  std::filesystem::path texturesPath_;
+};
+}  // namespace FW::MM
+#endif  // #ifndef FW_MULTIMEDIA_TEXTUREMANAGER_H
