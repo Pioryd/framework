6c2083dedead94decc22e67d244d61e31fc0a1e3
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 15:29:12 +0200

    Update time.cpp

--- src/framework/time/time.cpp
+++ src/framework/time/time.cpp
@@ -1,3 +1,23 @@
 #include "time.h"
 
-namespace FW::Time {}  // namespace FW::Time
+namespace FW::Time {
+Time::Time() : micros_(0), millis_(0), seconds_(0) {}
+
+void Time::resetToCurrentTime() {}
+
+void Time::resetToMicros(ticks_t newMicros) {}
+
+void Time::resetToMillis(ticks_t newMillis) {}
+
+void Time::resetToSecond(ticks_t newSeconds) {}
+
+ticks_t Time::toMicros() { return 0; }
+
+ticks_t Time::toMillis() { return 0; }
+
+ticks_t Time::toSeconds() { return 0; }
+
+void Time::setNull() {}
+
+bool Time::isNull() { return false; }
+}  // namespace FW::Time
