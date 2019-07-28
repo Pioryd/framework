9c21fbc21404e159a07737e4d9d5b0a3aa52f796
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 16:45:17 +0200

    Create applicationconsole.h

---
+++ src/framework/core/applicationconsole.h
@@ -0,0 +1,11 @@
+#ifndef FW_CORE_APPLICATION_CONSOLE_H
+#define FW_CORE_APPLICATION_CONSOLE_H
+
+#include "declarations.h"
+
+#include "applicationbase.h"
+
+namespace FW::Core {
+class ApplicationConsole : public ApplicationBase {};
+}  // namespace FW::Core
+#endif  // #ifndef FW_CORE_APPLICATION_CONSOLE_H
