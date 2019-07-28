fa44b834bd7d1ee55912f121c66c77d22d3c15b8
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 19:56:51 +0200

    Update protocol.cpp

--- src/framework/net/protocol.cpp
+++ src/framework/net/protocol.cpp
@@ -1,3 +1,19 @@
 #include "protocol.h"
 
-namespace FW::Net {}  // namespace FW::Net
+namespace FW::Net {
+Protocol::Protocol(
+    const std::string& name, std::function<void(Protocol_ptr&)> onConnected,
+    std::function<void(Protocol_ptr&, InputMessage&)> onPrsePacket) {}
+
+void Protocol::setConnection(const Connection_ptr& connection) {}
+
+void Protocol::prepareToWrite(const OutputMessage_ptr& msg) const {}
+
+void Protocol::onRecvMessage(InputMessage& msg) {}
+
+Connection_ptr Protocol::getConnection() const { return nullptr; }
+
+void Protocol::send(const OutputMessage_ptr& msg) const {}
+
+void Protocol::disconnect() const {}
+}  // namespace FW::Net
