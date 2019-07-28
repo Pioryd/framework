b15bb1bf1567c752351ce54589f71a453b3e04c7
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 16:45:25 +0200

    Update application.h

---
+++ src/framework/core/application.h
@@ -0,0 +1,17 @@
+#ifndef FW_CORE_APPLICATION_H
+#define FW_CORE_APPLICATION_H
+
+#include "declarations.h"
+
+#ifdef FW_DEF_GRAPHICAL_APPLICATION
+#include "applicationgraphical.h"
+#define APP_PARENT ApplicationGraphical
+#else
+#include "applicationconsole.h"
+#define APP_PARENT ApplicationConsole
+#endif
+
+namespace FW::Core {
+class Application : public APP_PARENT {};
+}  // namespace FW::Core
+#endif  // #ifndef FW_CORE_APPLICATION_H
