80a5d01ae2d8f8a4ad6c6e4a9f22b09df3ee6363
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 21:04:31 +0200

    Create global.h

---
+++ example/server/src/global.h
@@ -0,0 +1,11 @@
+#ifndef FW_SERVER_GLOBAL_H
+#define FW_SERVER_GLOBAL_H
+
+#include "declarations.h"
+
+namespace Server {
+namespace G {
+extern ServerApp serverApp;
+}  // namespace G
+}  // namespace Server
+#endif  // #ifndef FW_SERVER_GLOBAL_H
