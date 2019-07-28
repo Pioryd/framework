aeaaf73a563813b1273bd7c6536c7abdd4e52377
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 16:07:00 +0200

    Create mariadbmanager.h

---
+++ src/framework/database/mariadbmanager.h
@@ -0,0 +1,7 @@
+#ifndef FW_DATABASE_MARIADBMANAGER_H
+#define FW_DATABASE_MARIADBMANAGER_H
+
+#include "declarations.h"
+
+namespace FW::Database {}  // namespace FW::Database
+#endif                     // #ifndef FW_DATABASE_MARIADBMANAGER_H
