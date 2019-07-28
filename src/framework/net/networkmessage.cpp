ce3892f65d0d3ed5c3418e77a4c164d0119911c7
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 19:56:58 +0200

    Update networkmessage.cpp

--- src/framework/net/networkmessage.cpp
+++ src/framework/net/networkmessage.cpp
@@ -1,3 +1,18 @@
 #include "networkmessage.h"
 
-namespace FW::Net {}  // namespace FW::Net
+namespace FW::Net {
+NetworkMessage::NetworkMessage() {}
+
+uint16_t NetworkMessage::getLength() const { return 0; }
+void NetworkMessage::setLength(uint16_t length) {}
+
+uint8_t* NetworkMessage::getBuffer() { return 0; }
+
+const uint8_t* NetworkMessage::getBuffer() const { return 0; }
+
+uint8_t* NetworkMessage::getBufferAtCurrentPosition() { return 0; }
+
+const uint8_t* NetworkMessage::getBufferAtCurrentPosition() const { return 0; }
+
+uint16_t NetworkMessage::getBufferPosition() const { return 0; }
+}  // namespace FW::Net
