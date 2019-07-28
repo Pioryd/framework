7b51b1fea3bbb7d2173837200e8ba2d7b2bc5608
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 14:22:44 +0200

    Create math.h

---
+++ src/framework/util/math.h
@@ -0,0 +1,9 @@
+#ifndef FW_UTIL_MATH_H
+#define FW_UTIL_MATH_H
+
+#include "declarations.h"
+
+namespace FW::Util {
+namespace Math {}  // namespace Math
+}  // namespace FW::Util
+#endif  // FW_UTIL_MATH_H
