0598c2911487ff0d2d0ff83e912c25c406b13fd7
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 20:23:51 +0200

    Update global.cpp

--- src/framework/global.cpp
+++ src/framework/global.cpp
@@ -1,8 +1,35 @@
 #include "global.h"
 
+#include "core/application.h"
 #include "core/logger.h"
+#include "crypt/rsa.h"
+#include "pymodule/manager.h"
+#include "pymodule/signalmanager.h"
+#include "thread/eventmanager.h"
+#include "time/clock.h"
+
+#ifdef FW_DEF_GRAPHICAL_APPLICATION
+#include "multimedia/fontmanager.h"
+#include "multimedia/soundmanager.h"
+#include "multimedia/imagemanager.h"
+#include "multimedia/texturemanager.h"
+#include "multimedia/window.h"
+#endif
 
 namespace FW::G {
-FW::Core::Logger Logger;
+FW::Core::Logger Logger("FW::G::Logger");
+FW::Time::Clock Clock;
+FW::Crypt::Rsa Rsa(1024);
+FW::Thread::EventManager eventManager("FW::G::eventManager");
+
+FW::Thread::LookAfter<FW::PyModule::Manager> PyModuleManager;
 FW::Thread::LookAfter<FW::Core::Application> Application;
+
+#ifdef FW_DEF_GRAPHICAL_APPLICATION
+FW::MM::SoundManager SoundManager;
+FW::MM::FontManager FontManager;
+FW::MM::ImageManager ImageManager;
+FW::MM::TextureManager TextureManager;
+FW::MM::Window Window;
+#endif
 }  // namespace FW::G
