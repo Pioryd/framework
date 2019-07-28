afb441e6db9160442c7fc72eccc64cbc49b233dd
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 18:20:42 +0200

    Update signalmanager.cpp

--- src/framework/pymodule/signalmanager.cpp
+++ src/framework/pymodule/signalmanager.cpp
@@ -1,3 +1,34 @@
 #include "signalmanager.h"
 
-namespace FW::PyModule {}  // namespace FW::PyModule
+namespace FW::PyModule {
+SignalManager::SignalManager() {}
+
+bool SignalManager::connect(const std::string& signal, Callback_ptr callback) {
+  return false;
+}
+
+bool SignalManager::disconnect(const std::string& signal,
+                               Callback_ptr callback) {
+  return false;
+}
+
+bool SignalManager::disconnect(Module_ptr& module) {
+  return false;
+}
+
+void SignalManager::terminate() {
+}
+
+bool SignalManager::isConnected(const std::string& signal,
+                                Callback_ptr& callback) {
+  return false;
+}
+
+bool SignalManager::contains(const std::string& signal) const {
+  return false;
+}
+
+bool SignalManager::empty() const { return false; }
+
+void SignalManager::clear() { }
+}  // namespace FW::PyModule
