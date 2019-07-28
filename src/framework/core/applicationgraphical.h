7d6a229edbde32a34441b56eaeb5cca08936e50c
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 16:45:22 +0200

    Create applicationgraphical.h

---
+++ src/framework/core/applicationgraphical.h
@@ -0,0 +1,11 @@
+#ifndef FW_CORE_APPLICATION_GRAPHICAL_H
+#define FW_CORE_APPLICATION_GRAPHICAL_H
+
+#include "declarations.h"
+
+#include "applicationbase.h"
+
+namespace FW::Core {
+class ApplicationGraphical : public ApplicationBase {};
+}  // namespace FW::Core
+#endif  // #ifndef FW_CORE_APPLICATION_GRAPHICAL_H
