e14abcbd7fa27623e8453efe5621633b2eea64d4
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 19:14:57 +0200

    Update window.cpp

--- src/framework/multimedia/window.cpp
+++ src/framework/multimedia/window.cpp
@@ -1,3 +1,25 @@
 #include "window.h"
 
-namespace FW::MM {}  // namespace FW::MM
+#include "../global.h"
+
+namespace FW::MM {
+void Window::init(MM::SizeInt size, const std::string& name, bool fullScreen,
+                  uint32_t fpsLimit) {}
+
+void Window::poll() {}
+
+void Window::draw(const sf::Drawable& drawable) {}
+
+void Window::draw(const Color& color, const RectInt& rect) {}
+
+void Window::draw(const Color& color, const RectInt& rect,
+                  const Texture_ptr texture, const RectInt& textureRect) {}
+
+void Window::clear(FW::MM::Color color) {}
+
+void Window::display() {}
+
+bool Window::isOpen() { return false; }
+
+SizeInt Window::getSize() { return SizeInt(); }
+}  // namespace FW::MM
