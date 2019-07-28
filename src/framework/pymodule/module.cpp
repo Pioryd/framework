8becf414647f48faa1bd47d22a97430f45e3b84d
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 18:21:49 +0200

    Update module.cpp

--- src/framework/pymodule/module.cpp
+++ src/framework/pymodule/module.cpp
@@ -1,14 +1,26 @@
 #include "module.h"
 
+#include "callback.h"
+
+#include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string/replace.hpp>
+
 namespace FW::PyModule {
 Module::Module(const std::string& fileNameWithoutExtension,
-               const std::string& pathWithoutExtension, bool isPackage) {}
+               const std::string& pathWithoutExtension, bool isPackage)
+    : fileNameWithoutExtension_(fileNameWithoutExtension),
+      pathWithoutExtension_(pathWithoutExtension),
+      isPackage_(isPackage) {}
 
-std::string Module::getName() const { return ""; }
+std::string Module::getName() const { return fileNameWithoutExtension_; }
 
-std::string Module::getPathWithoutExtension() const { return ""; }
+std::string Module::getPathWithoutExtension() const {
+  return pathWithoutExtension_;
+}
 
-pybind11::module Module::getObject() const { return pybind11::module; }
+pybind11::module Module::getObject() const { return object_; }
 
-void Module::setObject(const pybind11::module& newObject) {}
+void Module::setObject(const pybind11::module& newObject) {
+  object_ = newObject;
+}
 }  // namespace FW::PyModule
