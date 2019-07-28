bba8c9a9e385054b183fa4d3ef25d73156dee209
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 19:56:54 +0200

    Update outputmessage.cpp

--- src/framework/net/outputmessage.cpp
+++ src/framework/net/outputmessage.cpp
@@ -1,3 +1,31 @@
 #include "outputmessage.h"
 
-namespace FW::Net {}  // namespace FW::Net
+namespace FW::Net {
+OutputMessage::OutputMessage() : NetworkMessage() {}
+
+void OutputMessage::reset() {}
+
+void OutputMessage::putUInt8(uint8_t value) {}
+
+void OutputMessage::putUInt16(uint16_t value) {}
+
+void OutputMessage::putUInt32(uint32_t value) {}
+
+void OutputMessage::putUInt64(uint64_t value) {}
+
+void OutputMessage::putInt8(int8_t value) {}
+
+void OutputMessage::putInt16(int16_t value) {}
+
+void OutputMessage::putInt32(int32_t value) {}
+
+void OutputMessage::putInt64(int64_t value) {}
+
+void OutputMessage::addString(const std::string& value) {}
+
+void OutputMessage::putBytes(const char* bytes, size_t size) {}
+
+void OutputMessage::addPaddingBytes(size_t count) {}
+
+void OutputMessage::throwIfCantPut(size_t size) const {}
+}  // namespace FW::Net
