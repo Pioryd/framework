1acd687f593818d7e58e430eb36d75158084152e
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 17:47:26 +0200

    Create manager.h

---
+++ src/framework/pymodule/manager.h
@@ -0,0 +1,7 @@
+#ifndef FW_PYMODULE_MANAGER_H
+#define FW_PYMODULE_MANAGER_H
+
+#include "declarations.h"
+
+namespace FW::PyModule {}  // namespace FW::PyModule
+#endif                     // #ifndef FW_PYMODULE_MANAGER_H
