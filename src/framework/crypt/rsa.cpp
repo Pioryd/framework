6f8add5d0e7d523e7c1e5cae8dfbf44b738d9303
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 15:43:27 +0200

    Update rsa.cpp

--- src/framework/crypt/rsa.cpp
+++ src/framework/crypt/rsa.cpp
@@ -1,1 +1,29 @@
 #include "rsa.h"
+
+using namespace FW::Crypt;
+
+#include <cryptopp/base64.h>
+#include <cryptopp/osrng.h>
+
+Rsa::Rsa(std::size_t const keySize) : keySize(keySize) {}
+
+Rsa::~Rsa() {}
+
+void Rsa::encrypt(unsigned char *msg) const {}
+
+void Rsa::decrypt(unsigned char *msg) const {}
+
+void Rsa::generatePrivateAndPublicKey(std::stringstream &ss) const {}
+
+void Rsa::setPublicKey(const char *n, const char *e) {}
+
+void Rsa::setPrivateKey(const char *n, const char *e, const char *d) {}
+
+std::string Rsa::getAsString(CryptoPP::RSA::PublicKey const &publicKey) const {
+  return "";
+}
+
+std::string Rsa::getAsString(
+    CryptoPP::RSA::PrivateKey const &privateKey) const {
+  return "";
+}
