8f18138b70311aef1212b9446b3cae2f611bdebc
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 19:15:07 +0200

    Update rectsides.h

--- src/framework/multimedia/rectsides.h
+++ src/framework/multimedia/rectsides.h
@@ -1,5 +1,22 @@
 #ifndef FW_MULTIMEDIA_RECTSIDES_H
 #define FW_MULTIMEDIA_RECTSIDES_H
 
-namespace FW::MM {}
+// Can't include, becouse current file is included in declarations.
+//#include "declarations.h"
+
+#include <SFML/Graphics.hpp>
+#include <type_traits>
+#include "../pch.h"
+
+namespace FW::MM {
+template <typename T = int>
+struct RectSides {
+  RectSides() { top = left = right = bottom = T(0); }
+  RectSides(T value) { top = left = right = bottom = value; }
+  T top;
+  T left;
+  T right;
+  T bottom;
+};
+}  // namespace FW::MM
 #endif  // #ifndef FW_MULTIMEDIA_RECTSIDES_H
