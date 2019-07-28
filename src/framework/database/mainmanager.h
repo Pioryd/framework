19a7ad6c9ce34774292ee4d5e627ba1262b7b0cf
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 16:07:09 +0200

    Create mainmanager.h

---
+++ src/framework/database/mainmanager.h
@@ -0,0 +1,7 @@
+#ifndef FW_DATABASE_MAINMANAGER_H
+#define FW_DATABASE_MAINMANAGER_H
+
+#include "declarations.h"
+
+namespace FW::Database {}  // namespace FW::Database
+#endif                     // FW_DATABASE_MAINMANAGER_H
