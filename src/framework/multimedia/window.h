dbe77ffffc632a08df7831c58dc7d8cac0289838
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 19:14:55 +0200

    Update window.h

--- src/framework/multimedia/window.h
+++ src/framework/multimedia/window.h
@@ -3,5 +3,35 @@
 
 #include "declarations.h"
 
-namespace FW::MM {}  // namespace FW::MM
-#endif               // #ifndef FW_MULTIMEDIA_WINDOW_H
+#include "color.h"
+#include "point.h"
+#include "rect.h"
+#include "size.h"
+
+#include <SFML/Graphics.hpp>
+#include <TGUI/TGUI.hpp>
+
+namespace FW::MM {
+class Window {
+ public:
+  void init(MM::SizeInt size, const std::string& name, bool fullScreen,
+            uint32_t fpsLimit);
+  void poll();
+
+  void draw(const sf::Drawable& drawable);
+  void draw(const Color& color, const RectInt& rect);
+  void draw(const Color& color, const RectInt& rect, const Texture_ptr texture,
+            const RectInt& textureRect);
+
+  void clear(FW::MM::Color color);
+  void display();
+
+  bool isOpen();
+  SizeInt getSize();
+
+ public:
+  std::unique_ptr<sf::RenderWindow> window;
+  std::unique_ptr<tgui::Gui> gui;
+};
+}  // namespace FW::MM
+#endif  // #ifndef FW_MULTIMEDIA_WINDOW_H
