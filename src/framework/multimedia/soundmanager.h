54e95f3ecf2b8c794300c5c562c3ea2fd026c139
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 18:29:21 +0200

    Create soundmanager.h

---
+++ src/framework/multimedia/soundmanager.h
@@ -0,0 +1,7 @@
+#ifndef FW_MULTIMEDIA_SOUNDMANAGER_H
+#define FW_MULTIMEDIA_SOUNDMANAGER_H
+
+#include "declarations.h"
+
+namespace FW::MM {}  // namespace FW::MM
+#endif               // #ifndef FW_MULTIMEDIA_SOUNDMANAGER_H
