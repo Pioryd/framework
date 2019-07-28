ad4578e4dacd90c1c7231b7169f9dd1c162b9426
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 17:32:53 +0200

    Update declarations.h

--- src/framework/core/declarations.h
+++ src/framework/core/declarations.h
@@ -5,5 +5,11 @@
 
 namespace FW::Core {
 class Logger;
+class Application;
+#ifdef FW_DEF_GRAPHICAL_APPLICATION
+class ApplicationGraphical;
+#else
+class ApplicationConsole;
+#endif
 }  // namespace FW::Core
 #endif  // #ifndef FW_CORE_DECLARATIONS_H
