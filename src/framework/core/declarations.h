61d493615bd6ca8a74b288b17b9edb140493fe9f
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 15:52:29 +0200

    Create declarations.h

---
+++ src/framework/core/declarations.h
@@ -0,0 +1,9 @@
+#ifndef FW_CORE_DECLARATIONS_H
+#define FW_CORE_DECLARATIONS_H
+
+#include "../pch.h"
+
+namespace FW::Core {
+class Logger;
+}  // namespace FW::Core
+#endif  // #ifndef FW_CORE_DECLARATIONS_H
