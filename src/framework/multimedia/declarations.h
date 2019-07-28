15cf5e07ce2bcbb76000dcea7e5daed552cb952e
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 19:15:36 +0200

    Update declarations.h

--- src/framework/multimedia/declarations.h
+++ src/framework/multimedia/declarations.h
@@ -3,5 +3,34 @@
 
 #include "../pch.h"
 
-namespace FW::MM {}  // namespace FW::MM
-#endif               // #ifndef FW_MULTIMEDIA_DECLARATIONS_H
+#include "point.h"
+#include "rect.h"
+#include "rectsides.h"
+#include "size.h"
+
+namespace sf {
+class Image;
+class Texture;
+class Font;
+class SoundBuffer;
+}  // namespace sf
+
+namespace FW::MM {
+class FontManager;
+class ImageManager;
+class SoundManager;
+class TextureManager;
+class Window;
+
+typedef sf::Image Image;
+typedef sf::Texture Texture;
+typedef sf::Font Font;
+typedef sf::SoundBuffer SoundBuffer;
+
+typedef std::shared_ptr<Image> Image_ptr;
+typedef std::shared_ptr<Texture> Texture_ptr;
+typedef std::shared_ptr<Font> Font_ptr;
+typedef std::shared_ptr<SoundBuffer> SoundBuffer_ptr;
+}  // namespace FW::MM
+
+#endif  // #ifndef FW_MULTIMEDIA_DECLARATIONS_H
