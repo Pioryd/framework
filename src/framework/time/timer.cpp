19b02e2bfde012ba63236953ad26142ec6b1c60d
Piotr Rydwański <pioryd123@gmail.com>
2019-07-28 15:29:09 +0200

    Update timer.cpp

--- src/framework/time/timer.cpp
+++ src/framework/time/timer.cpp
@@ -1,3 +1,26 @@
 #include "timer.h"
 
-namespace FW::Time {}  // namespace FW::Time
+namespace FW::Time {
+Timer::Timer() : running_{false} {}
+
+void Timer::start() {}
+
+void Timer::stop() {}
+
+void Timer::reset() {}
+
+bool Timer::isRunning() { return false; }
+
+void Timer::setCountdownInMicros(ticks_t micros) {}
+
+void Timer::setCountdownInMillis(ticks_t millis) {}
+
+void Timer::setCountdownInSecond(ticks_t seconds) {}
+
+ticks_t Timer::remainingTimeInMicros() { return 0; }
+
+ticks_t Timer::remainingTimeInMillis() { return 0; }
+
+ticks_t Timer::remainingTimeInSeconds() { return 0; }
+
+}  // namespace FW::Time
