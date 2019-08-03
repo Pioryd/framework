c556a4e0c1e665c0e1fe938e1853108b0ffe0d46
Piotr Rydwański <pioryd123@gmail.com>
2019-08-03 09:41:04 +0200

    Remove unneeded files.

--- example/client/src/declarations.h
+++
@@ -1,9 +0,0 @@
-#ifndef CLIENT_DECLARATIONS_H
-#define CLIENT_DECLARATIONS_H
-
-#include <framework/pch.h>
-
-namespace Client {
-class ClientApp;
-}  // namespace Client
-#endif  // #ifndef CLIENT_DECLARATIONS_H
--- example/client/src/main.cpp
+++ example/client/src/main.cpp
@@ -1,16 +1,7 @@
 #include <framework/core/application.h>
 #include <framework/global.h>
 
-#include "clientapp.h"
-#include "global.h"
-
-#include <modules/init.h>
-
 int main(int argc, char* argv[]) {
-  FW::G::Application->signals.onInit.connect(
-      "Server::G::serverApp", &Client::ClientApp::init, &Client::G::clientApp);
-  FW::G::Application->signals.onInit.connect("Modules::init", &Modules::init);
-
-  FW::G::Application->start(argc, argv, Client::G::clientApp.config.appName);
+  FW::G::Application->start(argc, argv, "Client");
   return 0;
 }
