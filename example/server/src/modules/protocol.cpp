e0de3b12c77048d0cbd4680bd266b6e5a39f5f13
Piotr Rydwański <pioryd123@gmail.com>
2019-08-03 11:26:53 +0200

    Create protocol.cpp

---
+++ example/server/src/modules/protocol.cpp
@@ -0,0 +1,13 @@
+#include <cmsext/log.h>
+#include <cmsext/module.h>
+#include <cmsext/net.h>
+#include <cmsext/thread.h>
+
+using namespace cmsext::net;
+using namespace cmsext::log;
+using namespace cmsext::thread;
+
+CMS_BEGIN(protocol_mod)
+
+
+CMS_END()
