7fae02b41992e11b8cfd48f83b7d140cd309db81
Piotr Rydwański <pioryd123@gmail.com>
2019-08-03 11:35:46 +0200

    Add module functionality to cmsext.

---
+++ src/cmsext/module.cpp
@@ -0,0 +1,45 @@
+#include "module.h"
+
+#include <framework/core/application.h>
+#include <framework/global.h>
+
+#include <framework/pymodule/manager.h>
+#include <pybind11/embed.h>
+
+namespace cmsext::module {
+void load(const cms::string& module_name) {
+  FW::G::PyModuleManager->loadAndImportModule(module_name);
+}
+
+void reload(const cms::string& module_name) {
+  FW::G::PyModuleManager->reloadModule(module_name);
+}
+
+void restartModules() { FW::G::PyModuleManager->restartModules(); }
+
+PYBIND11_EMBEDDED_MODULE(cmsext_module, m) {
+  m.def("load", &cmsext::module::load),  //
+      m.def("reload", &cmsext::module::reload),
+      m.def("restartModules", &cmsext::module::restartModules),
+      m.def("connect", &cmsext::module::restartModules),
+      m.def("disconnect", &cmsext::module::restartModules),
+      m.def(
+          "connect",
+          [](const std::string& signal, pybind11::object function,
+             pybind11::object* globalVariable = nullptr) {
+            FW::G::PyModuleManager->connect(signal, function, globalVariable);
+          },
+          pybind11::arg("signal"), pybind11::arg("object"),
+          pybind11::arg("globalVariable") = (pybind11::object*)nullptr),
+
+      m.def(
+          "disconnect",
+          [](const std::string& signal, pybind11::object function,
+             pybind11::object* globalVariable = nullptr) {
+            FW::G::PyModuleManager->disconnect(signal, function,
+                                               globalVariable);
+          },
+          pybind11::arg("signal"), pybind11::arg("object"),
+          pybind11::arg("globalVariable") = (pybind11::object*)nullptr);
+}
+}  // namespace cmsext::module
---
+++ src/cmsext/module.h
@@ -0,0 +1,52 @@
+#ifndef FW_CMSEXT_MODULE_H
+#define FW_CMSEXT_MODULE_H
+
+#include <cmsext/declarations.h>
+
+#include <framework/core/signal.h>
+#include <framework/pch.h>
+
+namespace cmsext::module {
+void load(const cms::string& module_name);
+void reload(const cms::string& module_name);
+void restartModules();
+
+template <typename T>
+void connect(const cms::string& signal_id, T callbackCpp) {
+  if (FW::Core::Signal<T>::signals.count(signal_id)) {
+    std::stringstream callbackd_id;
+    callbackd_id << (std::string)signal_id
+                 << static_cast<const void*>(callbackCpp);
+    FW::Core::Signal<T>::signals[(std::string)signal_id]->connect(
+        callbackd_id.str(), callbackCpp);
+  }
+};
+template <typename Obj, typename... Args>
+bool connect(const cms::string& signal_id, void (Obj::*objectFunction)(Args...),
+             Obj* object) {
+  if (FW::Core::Signal<T>::signals.count(signal_id)) {
+    std::stringstream callbackd_id;
+    callbackd_id << (std::string)signal_id
+                 << static_cast<const void*>(objectFunction)
+                 << static_cast<const void*>(object);
+
+    FW::Core::Signal<T>::signals[(std::string)signal_id]->connect(
+        callbackd_id.str(), objectFunction, object);
+  }
+}
+/*
+TODO
+Test with std::bind, std::function and cpp pointers.
+*/
+template <typename T>
+void disconnect(const cms::string& signal_id, T callbackCpp) {
+  if (FW::Core::Signal<T>::signals.count(signal_id)) {
+    std::stringstream callbackd_id;
+    callbackd_id << signal_id << static_cast<const void*>(callbackCpp);
+
+    FW::Core::Signal<T>::signals[signal_id]->disconnect(callbackd_id.str());
+  }
+}
+}  // namespace cmsext::module
+
+#endif  // #ifndef FW_CMSEXT_MODULE_H
