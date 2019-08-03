8f81e061b4b74ded629de66f370d5d5d1e4c5fe4
Piotr Rydwański <pioryd123@gmail.com>
2019-08-03 11:16:42 +0200

    Update signal.h

--- src/framework/core/signal.h
+++ src/framework/core/signal.h
@@ -13,41 +13,48 @@ namespace FW::Core {
 template <typename T>
 class Signal {
  public:
-  Signal(const std::string& pySignalId) : pySignalId_{pySignalId} {}
+  Signal(const std::string& pySignalId) : signal_id_{pySignalId} {
+    signals[pySignalId] = this;
+  }
 
-  bool connect(const std::string& id, T callbackCpp) {
-    if (callbacks_.count(id)) return false;
-    callbacks_[id] = callbackCpp;
+  bool connect(const std::string& cpp_callback_id, T cpp_callback) {
+    if (cpp_callbacks_.count(cpp_callback_id)) return false;
+    cpp_callbacks_[cpp_callback_id] = cpp_callback;
     return true;
   }
   template <typename Obj, typename... Args>
-  bool connect(const std::string& id, void (Obj::*objectFunction)(Args...),
-               Obj* object) {
+  bool connect(const std::string& cpp_callback_id,
+               void (Obj::*objectFunction)(Args...), Obj* object) {
     std::function<void(Args...)> function = [object,
                                              objectFunction](auto&&... args) {
       (object->*objectFunction)(args...);
     };
 
-    if (callbacks_.count(id)) return false;
-    callbacks_[id] = function;
+    if (cpp_callbacks_.count(cpp_callback_id)) return false;
+    cpp_callbacks_[cpp_callback_id] = function;
     return true;
   }
 
-  void disconnect(const std::string& id) { callbacks_.erase(id); }
+  void disconnect(const std::string& callback_id) {
+    cpp_callbacks_.erase(callback_id);
+  }
 
   template <typename... Args>
   void send(Args... args) {
-    for (auto& callback_pair : callbacks_) {
-      auto& callback = callback_pair.second;
-      callback(std::forward<Args>(args)...);
+    for (auto& cpp_callbacks_pair : cpp_callbacks_) {
+      auto& cpp_callback = cpp_callbacks_pair.second;
+      cpp_callback(std::forward<Args>(args)...);
     }
-    G::PyModuleManager->signalManager.send(pySignalId_,
+    G::PyModuleManager->signalManager.send(signal_id_,
                                            std::forward<Args>(args)...);
   }
 
+ public:
+  static inline std::map<std::string, Signal*> signals;  // <signal_id, signal>
+
  protected:
-  std::string pySignalId_;
-  std::map<std::string, T> callbacks_;
+  std::string signal_id_;
+  std::map<std::string, T> cpp_callbacks_;  // <callback_id, callback>
 };
 }  // namespace FW::Core
 #endif  // #ifndef FW_CORE_SIGNAL_H
