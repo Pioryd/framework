558921fabecf43339a8bbd2cbfad7a22e6c263e0
Piotr Rydwański <pioryd123@gmail.com>
2019-08-03 09:46:48 +0200

    Create ui.cpp

---
+++ example/client/src/modules/ui.cpp
@@ -0,0 +1,11 @@
+#include <cmsext/app.h>
+#include <cmsext/gui.h>
+#include <cmsext/widget.h>
+
+using namespace cmsext::widget;
+using namespace cmsext::app;
+using namespace cmsext::gui;
+
+CMS_BEGIN(ui_mod)
+
+CMS_END()
