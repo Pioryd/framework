75b132964b6b39281a62e96c9a5ec188b065e3e8
Piotr Rydwański <pioryd123@gmail.com>
2019-08-03 09:48:24 +0200

    Update protocol.cpp

--- example/client/src/modules/protocol.cpp
+++ example/client/src/modules/protocol.cpp
@@ -1,6 +1,6 @@
+#include <cmsext/log.h>
 #include <cmsext/module.h>
 #include <cmsext/net.h>
-#include <cmsext/log.h>
 #include <cmsext/thread.h>
 
 using namespace cmsext::net;
@@ -8,5 +8,51 @@ using namespace cmsext::log;
 using namespace cmsext::thread;
 CMS_BEGIN(test_mod)
 
+enum ProtocolCodes {
+  SERVER_PING = 0x05,
+  SERVER_PING_BACK = 0x06,
+  CLIENT_PING = 0x07,
+  CLIENT_PING_BACK = 0x08,
+  SERVER_TEXT_MESSAGE = 0x09,
+  CLIENT_TEXT_MESSAGE = 0x0A
+};
+
+void send_client_ping(uint32_t connection_id) {
+  paket_add_uint8(connection_id, CLIENT_PING);
+  paket_send(connection_id);
+
+  static int i = 0;
+  info("send CLIENT_PING " + std::to_string(++i));
+}
+void send_serer_ping_back(uint32_t connection_id) {
+  paket_add_uint8(connection_id, SERVER_PING_BACK);
+  paket_send(connection_id);
+
+  static int i = 0;
+  info("send SERVER_PING_BACK " + std::to_string(++i));
+}
+
+void parse_client_ping_back(uint32_t connection_id) {
+  add_sync_event(BIND(send_client_ping, connection_id), 500);
+}
+void parse_server_ping(uint32_t connection_id) {
+  add_sync_event(BIND(send_serer_ping_back, connection_id), 500);
+}
+
+void on_connected(uint32_t connection_id) { send_client_ping(connection_id); }
+
+void net_test() {
+  auto client_id = tcp_client_create("127.0.0.1", "7312", on_connected);
+
+  tcp_client_bind_paket_parse(client_id, CLIENT_PING_BACK,
+                              parse_client_ping_back);
+  tcp_client_bind_paket_parse(client_id, SERVER_PING, parse_server_ping);
+  tcp_client_connect(client_id);
+}
+// Signals
+void on_load() {}
+void on_init() { net_test(); }
+void on_terminate() {}
+void on_reload() {}
 
 CMS_END()
