b53dc150322c972c2d76b23bdcef85288cdba236
Piotr Rydwański <pioryd123@gmail.com>
2019-08-03 11:26:07 +0200

    Create ui.cpp

---
+++ example/server/src/modules/ui.cpp
@@ -0,0 +1,13 @@
+#include <cmsext/app.h>
+#include <cmsext/gui.h>
+#include <cmsext/widget.h>
+#include <cmsext/log.h>
+
+using namespace cmsext::widget;
+using namespace cmsext::app;
+using namespace cmsext::gui;
+using namespace cmsext::log;
+
+CMS_BEGIN(ui_mod)
+
+CMS_END()
