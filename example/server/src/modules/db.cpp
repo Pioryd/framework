629f2b47872dac5e5a56b7620c84a1e3d691aaeb
Piotr Rydwański <pioryd123@gmail.com>
2019-08-03 11:26:26 +0200

    Create db.cpp

---
+++ example/server/src/modules/db.cpp
@@ -0,0 +1,13 @@
+#include <cmsext/db.h>
+#include <cmsext/log.h>
+#include <cmsext/net.h>
+#include <cmsext/thread.h>
+
+using namespace cmsext::db;
+using namespace cmsext::log;
+using namespace cmsext::thread;
+
+CMS_BEGIN(db_mod)
+
+
+CMS_END()
