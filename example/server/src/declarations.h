c0cc7995437e2c6da01b851fc9b5ed64e60f64ed
Piotr Rydwański <pioryd123@gmail.com>
2019-08-03 11:10:38 +0200

    Remove server unneeded files

--- example/server/src/declarations.h
+++
@@ -1,9 +0,0 @@
-#ifndef SERVER_DECLARATIONS_H
-#define SERVER_DECLARATIONS_H
-
-#include <framework/pch.h>
-
-namespace Server {
-class ServerApp;
-}  // namespace Server
-#endif  // #ifndef SERVER_DECLARATIONS_H
